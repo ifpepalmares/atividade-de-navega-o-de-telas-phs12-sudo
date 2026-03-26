09bb0d46bf9a7907adf9fe8cd00e2b1ce87d11fa
pedrohenrique <phs12@discente.ifpe.edu.br>
2026-03-26 10:20:16 -0300

    Add files via upload

---
+++ atividade/Color.kt
@@ -0,0 +1,11 @@
+package com.example.composenavigationapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val cor1A = Color(0xFFFFFFFF)
+val cor2A = Color(0xFFFFFFFF)
+val cor3A = Color(0xFFFFFFFF)
+
+val Purple40 = Color(0xFF009688)
+val PurpleGrey40 = Color(0xFF4CAF50)
+val Pink40 = Color(0xFF8BC34A)
